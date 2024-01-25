8056d83789ecfd23aca286df363a4c6bf13734af
sakthivelk97 <sakthivelmaddy97@gmail.com>
2024-01-25 11:55:52 +0530

    learn how to test in bun environment
    add, sub, mul, div, isPrime

--- src/main.js
+++ src/main.js
@@ -1,3 +1,30 @@
+//add
 export function add(a,b){
   return a+b;
+}
+
+// sub
+export function sub(a,b){
+  return a-b;
+}
+
+//multiply
+export function mul(a,b){
+  return a*b;
+}
+
+//divide
+export function div(a,b){
+  return a/b;
+}
+
+//isPrime
+export function isPrime(num){
+  for (let i=2; i<num; i++){
+    if(num%i ===0){
+      return false
+    } else{return true};
+  }
+
+
 }
--- src/main.test.js
+++ src/main.test.js
@@ -8,3 +8,27 @@ test("adding two numbers",()=>{
   expect(result).toBe(5)
 
 })
+
+//sub
+test("subtract two numbers", ()=>{
+  const result = main.sub(2,1)
+  expect(result).toBe(1)
+})
+
+//mul
+test("multiply two numbers", ()=>{
+  const result = main.mul(2,3)
+  expect(result).toBe(6)
+})
+
+//div
+test("divide two numbers", ()=>{
+  const result = main.div(4,2)
+  expect(result).toBe(2)
+})
+
+//isPrime
+test("isPrime",()=>{
+  const result = main.isPrime(17)
+  expect (result).toBe(true)
+})
